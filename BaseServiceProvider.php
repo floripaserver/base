c10010885520f4dd465dc4b8de52bccd9a1a97b3
Paulo Roberto da Silva <paulo@softemp.com.br>
2017-04-12 15:24:00 -0300

    primeiro commit

---
+++ BaseServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SoftEmp\Base;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Description of BaseServiceProvider
+ *
+ * @author paulo
+ */
+class BaseServiceProvider extends ServiceProvider {
+    //put your code here
+}
